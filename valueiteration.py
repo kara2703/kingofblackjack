d522dce75112a5352b73c48fc2e44d6b1930c13d
kara2703 <kabraanoushka02@gmail.com>
2023-05-08 18:51:03 -0400

    update value iteration #2

--- valueiteration.py
+++ valueiteration.py
@@ -45,19 +45,53 @@ def transition(player_total, dealer_card, action):
         else:
             return ((player_total, dealer_card, True), 0)
 
-# # Define the value iteration algorithm
-# def value_iteration(gamma=1.0, epsilon=0.0001):
-#     # Initialize the value function
-#     V = np.zeros(len(states))
+# Define the value iteration algorithm
+def value_iteration(gamma=1.0, epsilon=0.0001):
+    # Initialize the value function
+    V = np.zeros(len(states))
 
-#     while True:
-#         # Keep track of the maximum change in V
-#         delta = 0
+    while True:
+        # Keep track of the maximum change in V
+        delta = 0
 
-#         # Iterate over all states
-#         for i, state in enumerate(states):
-#             # Calculate the Q-value for each action
-#             Q = np.zeros(len(actions))
-#             for j, action in enumerate(actions):
-#                 next_state, reward = transition(*state, action)
-#                 Q[j] = reward + gamma *
+        # Iterate over all states
+        for i, state in enumerate(states):
+            # Calculate the Q-value for each action
+            Q = np.zeros(len(actions))
+            for j, action in enumerate(actions):
+                next_state, reward = transition(*state, action)
+                Q[j] = reward + gamma * V[states.index(next_state)]
+
+            # Update the value function for the current state
+            V_new = np.max(Q)
+            delta = max(delta, np.abs(V[i] - V_new))
+            V[i] = V_new
+
+        # Check for convergence
+        if delta < epsilon:
+            break
+
+    # Compute the optimal policy
+    policy = np.zeros(len(states))
+    for i, state in enumerate(states):
+        Q = np.zeros(len(actions))
+        for j, action in enumerate(actions):
+            next_state, reward = transition(*state, action)
+            Q[j] = reward + gamma * V[states.index(next_state)]
+        policy[i] = np.argmax(Q)
+
+    return V, policy
+
+# Test the value iteration algorithm
+V, policy = value_iteration()
+
+# Print the optimal policy
+print("Optimal Policy:")
+for i in range(10):
+    print("Dealer showing", i + 1)
+    print("Player hand\t", end="")
+    for j in range(12, 22):
+        state = (j, i + 1, False)
+        action = policy[states.index(state)]
+        print("H" if action == 1 else "S", end="")
+    print("\n")
